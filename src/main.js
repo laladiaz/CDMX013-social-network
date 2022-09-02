19b62820fe95dd18af6686d91582101631dc6271
laladiaz <mariaabelardadiaz@gmail.com>
2022-09-02 12:38:55 -0500

    adds the export to onNavigate in main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import { welcome } from './components/welcome.js';
 // define root as the div root written in index.html
 const root = document.getElementById('root');
 // define the object routes for routing
-/* const routes = {
+const routes = {
   '/': welcome,
-}; */
-root.appendChild(welcome());
+};
+export const onNavigate = (pathname) => {
+  window.history.pushState(
+    {},
+    pathname,
+    window.location.origin + pathname,
+  );
+  root.appendChild(welcome());
+};
+
+const component = routes[window.location.pathname];
+root.appendChild(component());
