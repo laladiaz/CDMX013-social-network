d0a0d2aabb8634338a8d4522e72ec5ba5540685f
laladiaz <mariaabelardadiaz@gmail.com>
2022-09-02 17:00:09 -0500

    adds the route for login in main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+import { login } from './components/login.js';
 import { welcome } from './components/welcome.js';
 // define root as the div root written in index.html
 const root = document.getElementById('root');
 // define the object routes for routing
 const routes = {
   '/': welcome,
+  '/login': login,
 };
 export const onNavigate = (pathname) => {
   window.history.pushState(
