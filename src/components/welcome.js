5fddf4ebbeae3ed94d119d7a3865f0c70d362e1f
laladiaz <mariaabelardadiaz@gmail.com>
2022-09-02 16:35:55 -0500

    deletes the login nodes from welcome.js

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,5 +1,4 @@
 // import { onNavigate } from '../main.js';
-import { googleButton } from './googleButton.js';
 
 export const welcome = () => {
   // section welcome
@@ -35,53 +34,7 @@ export const welcome = () => {
   // appends the buttons to the main tag
   mainWelcome.append(loginButton, signupButton);
 
-  // adds the buttons for login with email or google
-  const loginButtonsDiv = document.createElement('div');
-  loginButtonsDiv.setAttribute('class', 'login-buttons-div');
-  const emailLoginButton = document.createElement('button');
-  emailLoginButton.setAttribute('id', 'email-login-button');
-  emailLoginButton.textContent = 'Continue with email';
-  const googleLoginButton = document.createElement('div');
-  googleLoginButton.setAttribute('id', 'buttonDiv');
-
-  // appends the googleButton module to the google login button
-  googleLoginButton.append(googleButton());
-
-  // adds the buttons for login to their div
-  loginButtonsDiv.append(emailLoginButton, googleLoginButton);
-
-  // adds dialog and form for a login modal
-  const loginModal = document.createElement('dialog');
-  loginModal.setAttribute('class', 'login-modal');
-  const formLoginModal = document.createElement('form');
-  formLoginModal.setAttribute('id', 'form-login-modal');
-  const emailLoginInput = document.createElement('input');
-  emailLoginInput.setAttribute('type', 'text');
-  emailLoginInput.setAttribute('placeholder', 'email');
-  emailLoginInput.setAttribute('class', 'login-input');
-  const passLoginInput = document.createElement('input');
-  passLoginInput.setAttribute('placeholder', 'password');
-  passLoginInput.setAttribute('type', 'password');
-  passLoginInput.setAttribute('class', 'login-input');
-  const cancelLoginButton = document.createElement('button');
-  cancelLoginButton.setAttribute('class', 'modal-login-button');
-  cancelLoginButton.textContent = 'Back';
-  const submitLoginButton = document.createElement('button');
-  submitLoginButton.setAttribute('class', 'modal-login-button');
-  submitLoginButton.textContent = 'Enter';
-  
-  // appends the buttons and input text areas to form
-  formLoginModal.append(emailLoginInput, passLoginInput, cancelLoginButton, submitLoginButton);
-
-  // appends the form to the dialog tag
-  loginModal.appendChild(formLoginModal);
-
-  // click listener for the emailLoginButton to show the loginModal
-  emailLoginButton.addEventListener('click', () => {
-    sectionWelcome.appendChild(loginModal);
-  });
-
   // appends the header and main to section
-  sectionWelcome.append(headerWelcome, mainWelcome, loginButtonsDiv);
+  sectionWelcome.append(headerWelcome, mainWelcome);
   return sectionWelcome;
 };
