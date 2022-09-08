03356d620731a61902a2cfdd52ab3210ec8b20a5
Jean-LC <jeanettelc@outlook.com>
2022-09-08 13:32:44 -0500

    cleans auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,33 +1,5 @@
 // eslint-disable-next-line import/no-unresolved
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
 import { app } from './config.js';
-import { onNavigate } from '../main.js';
 
 export const auth = getAuth(app);
-
-// email sign in authentication
-export const createUser = (a, x, y) => {
-  createUserWithEmailAndPassword(a, x, y)  
-    .then((userCredential) => {
-    // Signed in
-      const user = userCredential.user;
-      console.log(user);
-      onNavigate('/home');
-      // ...
-    });
-  console.log(x, y);
-};
-
-/* export const emailAuth = () => {
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-    // Signed in
-      const user = userCredential.user;
-    // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    // ..
-    });
-}; */ 
