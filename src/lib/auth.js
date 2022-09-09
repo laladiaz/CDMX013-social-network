71f0a2d0bf66cf5ff55adcd073356e53d44aa768
Jean-LC <jeanettelc@outlook.com>
2022-09-09 10:43:17 -0500

    adds provider google to auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line import/no-unresolved
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
 import { app } from './config.js';
 
 export const auth = getAuth(app);
+
+export const provider = new GoogleAuthProvider();
