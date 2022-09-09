3c594807f6c403650ab6f8806490407e13820663
Jean-LC <jeanettelc@outlook.com>
2022-09-09 16:41:42 -0500

    creates the elements necessary in home

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,56 @@
 export const home = () => {
   const sectionHome = document.createElement('section');
-  const homeTitle = document.createElement('h2');
-  homeTitle.textContent = 'Home';
-  sectionHome.appendChild(homeTitle);
+
+  // Header of home
+  const headerHome = document.createElement('header');
+  headerHome.setAttribute('class', 'header-home');
+  // div for logo and home title
+  const divLogo = document.createElement('div');
+  divLogo.setAttribute('class', 'div-logo');
+  const logoB = document.createElement('img');
+  logoB.setAttribute('src', './img/Logo-B.png');
+  logoB.setAttribute('class', 'logoB-header');
+  const titleHeader = document.createElement('h2');
+  titleHeader.setAttribute('class', 'title-header');
+  titleHeader.textContent = 'HOME';
+
+  divLogo.append(logoB, titleHeader);
+
+  // div for create post
+  const divCreatePost = document.createElement('div');
+  divCreatePost.setAttribute('class', 'div-create-post');
+  const imageCreatePost = document.createElement('img');
+  imageCreatePost.setAttribute('src', './img/new-post.png');
+  imageCreatePost.setAttribute('class', 'image-create-post');
+  const titleCreatePost = document.createElement('p');
+  titleCreatePost.setAttribute('class', 'title-create-post');
+  titleCreatePost.textContent = 'share something';
+
+  divCreatePost.append(imageCreatePost, titleCreatePost);
+
+  headerHome.append(divLogo, divCreatePost);
+
+  // section main
+  const mainHome = document.createElement('main');
+  mainHome.setAttribute('class', 'home-main');
+  const sectionPosts = document.createElement('section');
+  sectionPosts.setAttribute('class', 'section-posts');
+  const navMenu = document.createElement('nav');
+  navMenu.setAttribute('class', 'nav-menu');
+  const imageHomeNav = document.createElement('img');
+  imageHomeNav.setAttribute('src', './img/home.png');
+  imageHomeNav.setAttribute('class', 'image-home-nav');
+  const imageSearchNav = document.createElement('img');
+  imageSearchNav.setAttribute('src', './img/search.png');
+  imageSearchNav.setAttribute('class', 'image-search-nav');
+  const imageUserNav = document.createElement('img');
+  imageUserNav.setAttribute('src', './img/user.png');
+  imageUserNav.setAttribute('class', 'image-profile-nav');
+
+  navMenu.append(imageHomeNav, imageSearchNav, imageUserNav);
+
+  mainHome.append(sectionPosts, navMenu);
+
+  sectionHome.append(headerHome, mainHome); 
   return sectionHome;
 };
