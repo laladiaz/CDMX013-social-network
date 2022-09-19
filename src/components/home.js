0cfe4ba33266b34186dcea5415a5a27537bd8e96
Jean-LC <jeanettelc@outlook.com>
2022-09-19 13:48:56 -0500

    adds modal to create new post

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-unresolved
-import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+// import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
 import { auth } from '../lib/auth.js';
 import { onNavigate } from '../main.js';
 
@@ -41,6 +41,45 @@ export const home = () => {
   mainHome.setAttribute('class', 'home-main');
   const sectionPosts = document.createElement('section');
   sectionPosts.setAttribute('class', 'section-posts');
+
+  // modal post
+  const newPost = document.createElement('dialog');
+  newPost.setAttribute('class', 'new-post');
+  const divUserNewPost = document.createElement('div');
+  divUserNewPost.setAttribute('class', 'div-user-new-post');
+  const userImage = document.createElement('img');
+  userImage.setAttribute('src', './img/user.png');
+  const emailUserNewPost = document.createElement('p');
+  emailUserNewPost.setAttribute('class', 'email-user-new-post');
+  emailUserNewPost.textContent = '';
+  const inputNewPost = document.createElement('input');
+  inputNewPost.setAttribute('class', 'input-new-post');
+  inputNewPost.setAttribute('type', 'text');
+  inputNewPost.setAttribute('placeholder', 'share something');
+  const divSavePost = document.createElement('div');
+  divSavePost.setAttribute('class', 'save-post-div');
+  const savePostButton = document.createElement('button');
+  savePostButton.setAttribute('class', 'save-post-button');
+  const savePostText = document.createElement('p');
+  savePostText.setAttribute('class', 'save-post-text');
+  savePostText.textContent = 'save';
+  const divCancelPost = document.createElement('div');
+  divCancelPost.setAttribute('class', 'cancel-post');
+  const cancelPostButton = document.createElement('button');
+  cancelPostButton.setAttribute('class', 'cancel-post-button');
+  const cancelPostText = document.createElement('p');
+  cancelPostText.setAttribute('class', 'cancel-post-text');
+  cancelPostText.textContent = 'cancel';
+  const divButtonsNewPost = document.createElement('div');
+  divButtonsNewPost.setAttribute('class', 'div-buttons-new-post');
+
+  divCancelPost.append(cancelPostButton, cancelPostText);
+  divUserNewPost.append(userImage, emailUserNewPost);
+  divButtonsNewPost.append(divCancelPost, divSavePost);
+  divSavePost.append(savePostButton, savePostText);
+  newPost.append(divUserNewPost, inputNewPost, divButtonsNewPost);
+
+  // nav menu
   const navMenu = document.createElement('nav');
   navMenu.setAttribute('class', 'nav-menu');
   const indicatorDiv = document.createElement('div');
@@ -62,19 +101,31 @@ export const home = () => {
 
   navMenu.append(indicatorDiv, imageSearchNav, imageUserNav);
 
-  onAuthStateChanged(auth, (user) => {
+  /* onAuthStateChanged(auth, (user) => {
     if (user) {
-      console.log('user login');
+      console.log('user login', user.email);
     } else {
       onNavigate('/');
     }
-  });
+  }); */
 
   imageUserNav.addEventListener('click', () => {
     onNavigate('/profile');
   });
 
-  mainHome.append(sectionPosts, navMenu);
+  // const user = auth.currentUser;
+  
+  console.log(auth); 
+
+  divCreatePost.addEventListener('click', () => {
+    newPost.showModal();
+    const user = auth.currentUser;
+    if (user) {
+      emailUserNewPost.innerHTML = user.email;
+    } 
+  }); 
+
+  mainHome.append(sectionPosts, navMenu, newPost);
 
   sectionHome.append(headerHome, mainHome); 
   return sectionHome;
