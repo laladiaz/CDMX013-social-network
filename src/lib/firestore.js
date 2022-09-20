15673a581ed0881483f7e833f1c870bff5659102
laladiaz <mariaabelardadiaz@gmail.com>
2022-09-20 18:48:09 -0500

    writes the savePosts function and the onGetPosts function in firestore.js

--- src/lib/firestore.js
+++ src/lib/firestore.js
@@ -1,5 +1,14 @@
+import {
+  getFirestore, collection, addDoc, onSnapshot, 
 // eslint-disable-next-line import/no-unresolved
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js';
+} from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js';
 import { app } from './config.js';
 
 export const db = getFirestore(app);
+
+// add to the posts collection
+export const savePost = (text) => addDoc(collection(db, 'posts'), { text });
+
+// to fetch the posts collection
+// export const getPosts = () => getDocs(db, 'posts');
+export const onGetPosts = (callback) => onSnapshot(collection(db, 'posts'), callback); 
