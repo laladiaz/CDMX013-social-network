3d50ea4d8c766dc25038a7dec733f3bece43fff7
Jean-LC <jeanettelc@outlook.com>
2022-09-28 11:03:25 -0500

    adds edit post function to firestore.js

--- src/lib/firestore.js
+++ src/lib/firestore.js
@@ -1,5 +1,5 @@
 import {
-  getFirestore, collection, addDoc, onSnapshot, query, orderBy, deleteDoc, doc,
+  getFirestore, collection, addDoc, onSnapshot, query, orderBy, deleteDoc, doc, getDoc, updateDoc,
 // eslint-disable-next-line import/no-unresolved
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js';
 import { app } from './config.js';
@@ -13,6 +13,11 @@ export const savePost = (email, text, time) => addDoc(collection(db, 'posts'), {
 
 // to fetch the posts collection
 const orderPost = query(collection(db, 'posts'), orderBy('time', 'desc'));
+
 export const onGetPosts = (callback) => onSnapshot(orderPost, callback); 
 
 export const deletePost = (id) => deleteDoc(doc(db, 'posts', id));
+
+export const getPost = (id) => getDoc(doc(db, 'posts', id));
+
+export const updatePost = (id, text) => updateDoc(doc(db, 'posts', id), text);
